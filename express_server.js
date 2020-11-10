733169283c62133ca911c4e520d4f62da4505c6b
SaraKate51115 <sarakate51115@gmail.com>
2020-11-10 03:41:14 +0000

    added a GET route to render the urls_new template in the browser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -27,7 +30,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const templateVars = {shortURL, longURL: urlDatabase[shortURL]};
