b587fe22d3c70f3a8a515deb83918c63f1681bb7
SaraKate51115 <sarakate51115@gmail.com>
2020-11-12 04:19:31 +0000

    added an endpoint to handle POST to /login that sets a cookie to an input username, updated existing routes to render templates as showing the username when logged in

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 //implementing function to simulate generating a "unique" shortURL
 const generateRandomString = function (length) {
@@ -14,6 +15,7 @@ const generateRandomString = function (length) {
   return result;
 };
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -33,6 +35,25 @@ const editURL = (url, shortURL) => {
 };
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// app.get("/urls", (req, res) => {
+//   const templateVars = {
+//     username: req.cookies['username'],
+//   };
+  
+//   res.render('urls_index', templateVars); //===> display in tempate?
+// });
+//~~~~~~~~~~~~~???????
+app.post("/login", (req, res) => {
+  //console.log(req.body, req.params);
+  res.cookie('username', req.body.username);
+  // const templateVars = {
+  //   username: req.body['username'],
+  // };
+  console.log(req.body['username'])
+  //res.render("urls_index", templateVars);
+  res.redirect('/urls');
+});
+//=====>
 app.post("/urls/:shortURL/edit", (req, res) => {
   console.log("request to edit from myURLs page: ", req.params.shortURL)
   const shortURL = req.params.shortURL;
@@ -67,18 +88,26 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies['username'],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
-  const templateVars = {shortURL, longURL: urlDatabase[shortURL]};
+  const templateVars = {shortURL, 
+    longURL: urlDatabase[shortURL],
+    username: req.cookies['username'],
+  };
   res.render("urls_show", templateVars);
 });
 
-//pass the url data to our template
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+    username: req.cookies['username'],
+   };
+   console.log(req.cookies)
   res.render("urls_index", templateVars);
 });
 
